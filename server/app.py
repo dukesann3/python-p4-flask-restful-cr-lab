d033a6ed60a3e91ed41874e81228a2da1b316378
Duke <dukemorise33@gmail.com>
2024-02-01 22:47:11 -0800

    completed lesson

--- server/app.py
+++ server/app.py
@@ -17,7 +17,55 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+
+        all_plants = [n.to_dict() for n in Plant.query.all()]
+
+        response = make_response(
+            all_plants,
+            200
+        )
+
+        return response
+    
+    def post(self):
+        req = request.get_json()
+
+        new_plant = Plant(
+            name=req["name"],
+            image=req["image"],
+            price=req["price"]
+        )
+
+        print(new_plant)
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        plant_dict = new_plant.to_dict()
+
+        response = make_response( 
+            plant_dict,
+            201
+        )
+
+        return response
+    
+api.add_resource(Plants, '/plants')
+
+class Plant_by_id(Resource):
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            plant,
+            200
+        )
+
+        return response
+    
+api.add_resource(Plant_by_id, '/plants/<int:id>')
+
 
 class PlantByID(Resource):
     pass
--- server/models.py
+++ server/models.py
@@ -7,3 +7,13 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return (
+            f"name: {self.name}"
+            f"image: {self.image}"
+            f"price: {self.price}"
+        )
